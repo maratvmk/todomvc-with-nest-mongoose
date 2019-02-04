a5fcb8da8496f0c116789eb4ce72522fd5e03d2f
Marat Kazbekov <maratvmk@mail.ru>
2019-02-05 01:00:39 +0600

    added HapiJS as a backend server

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Run all commands from root of the project.
 
 First of all run mongo with `docker-compose up -d`.
 
-Then run backend with `ng serve backend`.
+Run NestJS backend with `ng serve backend` or HapiJS backend with `ng serve hapi`.
 
 Then run frontend with `ng serve todomvc`.
 
---
+++ apps/hapi/src/app/tasks/task.service.ts
@@ -0,0 +1,28 @@
+import { TaskModel } from "./task";
+
+export class TaskService {
+    
+    async findAll(title?: string) {
+        if (title) {
+            return await TaskModel.find({title: { $regex: new RegExp(title, 'i') }}).exec();
+        }
+        else {
+            return await TaskModel.find().exec();
+        }
+    }
+
+    async create(task) {
+        const kitty = new TaskModel(task);
+        return await kitty.save();
+    }
+
+    async update(task) {
+        let t = await TaskModel.findById(task._id);
+        t.complete = task.complete;
+        return await t.save();
+    }
+
+    async delete(id) {
+        return await TaskModel.findByIdAndRemove(id).exec();
+    }
+}
